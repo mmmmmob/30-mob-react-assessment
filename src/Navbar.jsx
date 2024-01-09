0dd782a536f6089b613af2e82b2bcd94e4592b0b
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2024-01-09 11:05:00 +0700

    edited Navbar as in spec

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,18 +3,17 @@ import React from "react";
 const NavBar = () => {
   return (
     <div className="navbar sticky top-0 left-0 right-0 text-slate-900 bg-blue-400 dark:bg-blue-950 dark:text-slate-300">
-      <div className="flex-1">
-        <a className="btn btn-ghost text-xl" href="/">
-          JSD6 : React Assessment
-        </a>
-      </div>
-      <div className="flex-none">
+      <div className="flex-none ml-auto">
         <ul className="menu menu-horizontal px-1">
           <li>
-            <a href="/">Home</a>
+            <a className="text-lg" href="/">
+              Home
+            </a>
           </li>
           <li>
-            <a href="owner">Owner</a>
+            <a className="text-lg" href="owner">
+              Owner
+            </a>
           </li>
         </ul>
       </div>
