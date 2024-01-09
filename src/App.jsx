d412e130fd316b14d379462e0f63f73dde0ef5f2
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2024-01-09 14:44:49 +0700

    finished User component with Axios.get

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-import { Axios } from "axios";
+import Axios from "axios";
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import Layout from "./Layout";
 
 function App() {
+  const [employee, setEmployee] = useState([]);
+  const [sector, setSector] = useState("admin");
+
+  useEffect(() => {
+    const getEmployeeData = async () => {
+      const employeeData = await Axios.get(
+        "https://jsd5-mock-backend.onrender.com/members"
+      );
+      setEmployee([...employeeData.data]);
+    };
+    getEmployeeData();
+  }, []);
+
   return (
     <>
       <Layout>
@@ -13,27 +26,35 @@ function App() {
             <h1 className="text-5xl my-3">React Assessment</h1>
           </div>
           <div className="my-6 flex w-full justify-evenly">
-            <button className="btn btn-accent">Use Home Sector</button>
+            <button className="btn btn-primary">Use Home Sector</button>
             <button className="btn btn-warning">Admin Home Sector</button>
           </div>
-          {/* <Users /> */}
-          <Admin />
+          <Users employee={employee} />
+          {/* <Admin /> */}
         </div>
       </Layout>
     </>
   );
 }
 
-function Users() {
+function Users({ employee }) {
   return (
     <div className="overflow-x-auto my-4">
-      <table className="table">
+      <table className="table table-zebra">
         <thead className="text-lg">
           <td>Name</td>
           <td>Lastname</td>
           <td>Position</td>
         </thead>
-        <tbody>{/* td with .map here */}</tbody>
+        <tbody>
+          {employee.map((member) => (
+            <tr key={member.id}>
+              <td>{member.name}</td>
+              <td>{member.lastname}</td>
+              <td>{member.position}</td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   );
@@ -43,17 +64,29 @@ function Admin() {
   return (
     <div>
       <h1 className="text-xl font-semibold my-4">Create user here</h1>
-      <div className="flex flex-col justify-center items-center ">
-        <div className="my-4">
+      <div className="flex flex-col justify-center items-center">
+        <div className="mb-4 bt-2 flex justify-center items-center ">
           <form>
-            <input type="text" placeholder="Name" />
-            <input type="text" placeholder="Lastname" />
-            <input type="text" placeholder="Position" />
-            <button>Submit</button>
+            <input
+              className=" mr-4 input input-bordered input-primary "
+              type="text"
+              placeholder="Name"
+            />
+            <input
+              className="mx-3 input input-bordered input-primary "
+              type="text"
+              placeholder="Lastname"
+            />
+            <input
+              className="mx-3 input input-bordered input-primary "
+              type="text"
+              placeholder="Position"
+            />
+            <button className="mx-3 btn btn-success">Submit</button>
           </form>
         </div>
         <div className="overflow-x-auto my-4">
-          <table className="table">
+          <table className="table table-zebra">
             <thead className="text-lg">
               <td>Name</td>
               <td>Lastname</td>
